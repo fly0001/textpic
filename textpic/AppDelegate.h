07118985096169415c9accbdd63d29cf86538843
git009 <liuyong_000@163.com>
2018-10-21 14:17:16 +0800

    Initial Commit

---
+++ textpic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  textpic
+//
+//  Created by mac on 2018/10/21.
+//  Copyright © 2018年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
